63690e71d1979f5c5a96429250ddb8db58230fae
Justus Perlwitz <hello@justus.pw>
2024-01-02 10:11:55 +0900

    Remove "created" state
    Sarus will try to connect automatically in the constructor, so it is not
    necessary to distinguish between "created" and "connecting".

--- src/index.ts
+++ src/index.ts
@@ -117,24 +117,27 @@ export default class Sarus {
   ws: WebSocket | undefined;
   /*
    * Track the current state of the Sarus object. See the diagram below.
-   *          connect()        this.ws.onopen
-   * ┌───────┐ │  ┌──────────┐      │    ┌─────────┐
-   * │created│─┴─►│connecting│──────┴───►│connected│ ◄───────────────────┐
-   * └───────┘    └──────────┘           └─────────┘                     │
-   *                 disconnect()          │ ▲    │                      │
-   *      ┌────────────────────────────────┘ │    │ this.ws.onclose      │
-   *      ▼                                  │    ▼                      │
-   * ┌────────────┐    connect()             │ ┌──────┐                  │
-   * │disconnected│──────────────────────────┘ │closed│ ─────────────────┘
-   * └────────────┘                            └──────┘   this.reconnect()
+   *
+   *                       reconnect()    ┌──────┐
+   *      ┌───────────────────────────────│closed│
+   *      │                               └──────┘
+   *      │                                  ▲
+   *      ▼                                  │ this.ws.onclose
+   * ┌──────────┐    this.ws.onopen      ┌───┴─────┐
+   * │connecting├───────────────────────►│connected│
+   * └──────────┘                        └───┬─────┘
+   *      ▲                                  │ disconnect()
+   *      │                                  ▼
+   *      │            reconnect()      ┌────────────┐
+   *      └─────────────────────────────┤disconnected│
+   *                                    └────────────┘
    *
    * connect(), disconnect() are generally called by the user
    *
    * this.reconnect() is called internally when automatic reconnection is
    * enabled, but can also be called by the user
    */
-  state: "created" | "connecting" | "connected" | "disconnected" | "closed" =
-    "created";
+  state: "connecting" | "connected" | "disconnected" | "closed" = "connecting";
 
   constructor(props: SarusClassParams) {
     // Extract the properties that are passed to the class
